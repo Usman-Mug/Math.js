43150fd7149df2c981113bf05b0984caa887be72
Usman-Mug <143292226+Usman-Mug@users.noreply.github.com>
2023-09-21 05:21:15 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+a = +prompt("Enter your first number")
+b = +prompt("Enter your second number")
+c=a+b
+alert(c)
+document.write(c)
+
+
+a = -prompt('Enter your first number')
+b = -prompt('Enter your second number')
+c=a-b
+alert(c)
+document.write(c)
+
+a = -prompt('Enter your first number')
+b = -prompt('Enter your second number')
+c=a*b
+alert(c)
+document.write(c)
+
+a = -prompt('Enter your first number')
+b = -prompt('Enter your second number')
+c=a/b
+alert(c)
+document.write(c)
